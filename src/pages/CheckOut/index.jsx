ef0542727ef9d514006ce53dbe65ac3f658b323f
Phuong Minh Tran <tranphuongdev31397@gmail.com>
2022-05-15 14:31:36 +0700

    responsive checkout page

--- src/pages/CheckOut/index.jsx
+++ src/pages/CheckOut/index.jsx
@@ -3,7 +3,6 @@ import { PATH_IMG } from 'constant';
 import CheckOutLeftSide from 'containers/CheckOutLeftSide';
 import CheckOutRightSide from 'containers/CheckOutRightSide';
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
 
 function CheckOut() {
     return (
@@ -12,11 +11,11 @@ function CheckOut() {
                 titlePage={'Checkout'}
                 bgImg={PATH_IMG + 'pageBanner.jpg'}
             />
-            <div className="checkOut__container flex flex-start justify-between container mx-auto gap-6 my-10">
-                <div className="checkOut__leftSide w-2/3">
+            <div className="checkOut__container flex flex-col-reverse sm:flex-row flex-start justify-between container px-10 sm:px-0 mx-auto gap-6 my-10 ">
+                <div className="checkOut__leftSide w-full sm:w-2/3">
                     <CheckOutLeftSide />
                 </div>
-                <div className="checkOut__rightSide w-1/4">
+                <div className="checkOut__rightSide w-full sm:w-1/4">
                     <CheckOutRightSide />
                 </div>
             </div>
